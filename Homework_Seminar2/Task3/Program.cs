6eb5bcb87ff34228cf12fee2bebdc75be222a666
Dmitry <rasma1@mail.ru>
2024-03-15 17:41:15 +0300

    Homework_Seminr2_Task3

---
+++ Homework_Seminar2/Task3/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задача 3: Напишите программу, которая принимает
+// на вход целое число из отрезка [10, 99] и показывает
+// наибольшую цифру числа.
+// 40 => 4
+// 96 => 9
+// 72 => 7
+
+Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number < 10 || number > 99)
+{
+    Console.WriteLine("некорректный ввод");
+}
+else
+{
+    int firstDigit = number / 10;
+    int secondDigit = number % 10;
+    int max = firstDigit;
+    if (secondDigit > max)
+    {
+        max = secondDigit;
+    }
+    Console.WriteLine(max);
+}
