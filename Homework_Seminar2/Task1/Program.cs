1c04df1f612a52a0d3e16e905debfcc512ec32db
Dmitry <rasma1@mail.ru>
2024-03-15 17:40:39 +0300

    Homework_Seminr2_Task1

---
+++ Homework_Seminar2/Task1/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 1: Напишите программу, которая принимает
+// на вход число и проверяет, кратно ли оно
+// одновременно 7 и 23
+// a=50 => нет
+// a=7 => нет
+// a=322 => да
+
+Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number % 7 == 0 && number % 23 == 0)
+{
+    Console.WriteLine("Число кратно");
+}
+else
+{
+    Console.WriteLine("Число не кратно");
+}
