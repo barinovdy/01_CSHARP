11c94676152b7237823d427a750cf97efad3926a
Dmitry <rasma1@mail.ru>
2024-03-15 17:41:24 +0300

    Homework_Seminr2_Task4

---
+++ Homework_Seminar2/Task4/Program.cs
@@ -0,0 +1,37 @@
+﻿// Задача 4: Напишите программу, которая на вход
+// принимает натуральное число N, а на выходе
+// показывает его цифры через запятую.
+// 568 => 5,6,8
+// 8 => 8
+// 9542 => 9,5,4,2
+
+Console.WriteLine("Введите натуральное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number < 0)
+{
+    Console.WriteLine("Некорректный ввод");
+}
+else
+{
+    if (number < 10)
+    {
+        Console.WriteLine(number);
+    }
+    else
+    {
+        int rank = 10;
+        while (number / rank >= 10)
+        {
+            rank = rank *10;
+        }
+        int digit = number / rank;
+        while (rank >= 10)
+        {
+            Console.Write(digit + ",");
+            rank = rank / 10;
+            digit = number / rank % 10;
+        }
+        Console.Write(digit);
+    }
+}
