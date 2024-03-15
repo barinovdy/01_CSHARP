4d5cb1786ebca2f8347fd6a77bedc27093330d4f
Dmitry <rasma1@mail.ru>
2024-03-15 17:28:57 +0300

    Seminar1_Task1

---
+++ Seminar1/Task1/Program.cs
@@ -0,0 +1,23 @@
+﻿// Напишите   программу,   которая   на   вход принимает  два  
+// целых  числа  и  проверяет, является ли 
+// первое число квадратом второго.
+// a = 25, b = 5 => да
+// a = 2, b = 10         => нет
+// a = 9, b = -3         => да
+// a = -3, b = 9         => нет
+
+Console.WriteLine("Insert first number: ");
+string num1str = Console.ReadLine();
+int num1 = Convert.ToInt32(num1str);
+
+Console.WriteLine("Insert second number: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+if (num2 * num2 == num1)
+{
+    Console.WriteLine("Первое число является квадратом второго");
+}
+else
+{
+    Console.WriteLine("Первое число не является квадратом второго");
+}
