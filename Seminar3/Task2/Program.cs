e3cfcac94443df1380211afb9d9956f0f7046cff
Dmitry <rasma1@mail.ru>
2024-03-15 18:07:22 +0300

    Seminar3_Task2

---
+++ Seminar3/Task2/Program.cs
@@ -0,0 +1,35 @@
+﻿// Задайте массив из 10 элементов, заполненный числами из
+// промежутка [-10, 10]. Замените отрицательные элементы на
+// положительные, а положительные на отрицательные.
+// Пример
+// [1 -5 6] => [-1 5 -6]
+
+int[] array = new int[10] { -10, -7, -5, -1, 0, 4, 5, 7, 9 , 5};
+
+int length = array.Length;
+int index = 0;
+
+for (int i = 0; i < length; i++) //Печатаем массив с ',' и последний элемент без ','
+{
+    if (i < length - 1)
+    {
+        Console.Write($"{array[i]}, ");
+    }
+    else
+    {
+        Console.Write(array[i]);
+
+    }
+}
+
+Console.WriteLine();
+
+
+while (index < length)
+{
+    //array[index] = array[index] * -1;
+    array[index] *= -1;
+    Console.Write(array[index] + " ");
+    index++;
+}
+
