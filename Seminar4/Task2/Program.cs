49673c8aca7726d229d249d831197e2323f70794
Dmitry <rasma1@mail.ru>
2024-03-19 22:16:49 +0300

    Seminar4_Task2

---
+++ Seminar4/Task2/Program.cs
@@ -0,0 +1,48 @@
+﻿// Задайте массив из N случайных целых чисел (N вводится с
+// клавиатуры).
+// Найдите количество чисел, которые оканчиваются на 1 и
+// делятся нацело на 7.
+// Пример
+// [1 5 11 21 81 4 0 91 2 3]
+// => 2
+
+int[] CreateArrayRndInt(int size, int min, int max)
+{
+    int[] array = new int[size];
+    Random rnd = new Random();
+
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = rnd.Next(min, max);
+    }
+    return array;
+}
+
+void PrintArray(int[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine("]");
+}
+
+int NumberCountCheck(int[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] % 10 == 1 && array[i] % 7 == 0) count++;
+    }
+    return count;
+}
+
+
+Console.WriteLine("ВВедите число n: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+int[] newArray = CreateArrayRndInt(n, 0, 100);
+PrintArray(newArray);
+Console.WriteLine();
+Console.WriteLine(NumberCountCheck(newArray));
