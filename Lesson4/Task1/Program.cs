437646fe2464fa56301329b9de0b75c2930241c6
Dmitry <rasma1@mail.ru>
2024-03-20 22:08:04 +0300

    Lesson4_Task1

--- Lesson4/Task1/Program.cs
+++ Lesson4/Task1/Program.cs
@@ -1,19 +1,28 @@
-﻿int[,] matrix = new int[4, 5];
-
-Random rnd = new Random();
-for (int i = 0; i < matrix.GetLength(0); i++) //GetLength(0) - нулевое измерение - количество строк
+﻿int[,] CreateMatrix(int rowCount, int columnCount)
 {
-    for (int j = 0; j < matrix.GetLength(1); j++)
+    int[,] matrix = new int[rowCount, columnCount];
+    Random rnd = new Random();
+    for (int i = 0; i < matrix.GetLength(0); i++) //GetLength(0) - нулевое измерение - количество строк
     {
-        matrix[i, j] = rnd.Next(1, 11);
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rnd.Next(1, 11);
+        }
+    }
+    return matrix;
+}
+
+void ShowMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j]} ");
+        }
+        Console.WriteLine();
     }
 }
 
-for (int i = 0; i < matrix.GetLength(0); i++)
-{
-    for (int j = 0; j < matrix.GetLength(1); j++)
-    {
-        Console.Write($"{matrix[i, j]} ");
-    }
-    Console.WriteLine();
-}
+int[,] matrix = CreateMatrix(4,5);
+ShowMatrix(matrix);
