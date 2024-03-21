633d8c6290c6f726eb2bedf3910a592e3a56b5f8
Dmitry <rasma1@mail.ru>
2024-03-21 22:38:15 +0300

    Seminar 4 Homework

--- Homework_Seminar4/Task1/Program.cs
+++ Homework_Seminar4/Task1/Program.cs
@@ -5,31 +5,50 @@
 int SumOfDigits(int number)
 {
     int sum = 0;
-    while(number > 1)
+    while (number > 0)//457
     {
-        sum = sum + number%10;
-        number /=10;
+        sum = sum + number % 10;
+        number /= 10;
     }
     return sum;
+}
+
+bool IsNumber(string str)
+{
+    for (int i = 0; i < str.Length; i++)
+    {
+        if (char.IsAsciiDigit(str[i]) == false)
+        {
+            return false;
+        }
+    }
+    return true;
 }
 
 while (true)
 {
     string str = Console.ReadLine();
-    if(str =="q")
+    if (str == "q")
     {
         Console.WriteLine("Введен символ 'q'");
         break;
     }
-    
-    //if (введено число)
-    //{
+    if (IsNumber(str) == true)
+    {
         int number = Convert.ToInt32(str);
-        int sum = SumOfDigits(number); 
-        Console.WriteLine(sum);
-    //}
-    //if (введено не число)
-    //{
-    //    Console.WriteLine("Некорректный ввод");
-    //}
+        int sum = SumOfDigits(number);
+        if (sum % 2 == 0)
+        {
+            Console.WriteLine("Сумма цифр четная: " + sum);
+            break;
+        }
+        else
+        {
+            Console.WriteLine("Сумма цифр нечетная: " + sum);
+        }
+    }
+    else
+    {
+        Console.WriteLine("Некорректный ввод. Введите символ 'q' или число.");
+    }
 }
