f758e94520f73e010112d4a081b89e480d866146
Dmitry <rasma1@mail.ru>
2024-03-26 22:25:16 +0300

    Lesson5 Task1

---
+++ Lesson5/Task1/Program.cs
@@ -0,0 +1,14 @@
+﻿//Вычисление факториала числа через рекурсию
+
+int Fact(int n)
+{
+    if (n == 1 || n == 0)
+    {
+        Console.WriteLine($"Stop: {n}");
+        return 1;
+    }
+    Console.WriteLine(n);
+    return n * Fact(n - 1);
+}
+
+Console.WriteLine(Fact(5));
