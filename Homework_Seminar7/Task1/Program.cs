27c2423c4c92051b6826f619f3f6f8a956e79244
Dmitry <rasma1@mail.ru>
2024-03-28 23:11:26 +0300

    Homework_Seminar7_Task1

---
+++ Homework_Seminar7/Task1/Program.cs
@@ -0,0 +1,31 @@
+﻿// Задача 1: Задайте значения M и N. 
+// Напишите программу, которая выведет все натуральные числа 
+// в промежутке от M до N. 
+// Использовать рекурсию, не использовать циклы.
+
+void PrintNumbersBetweenMN(int numM, int numN)
+{
+    if (numM == numN) Console.Write($"{numM} ");
+    if (numM < numN) 
+    {
+        int num = numM;
+        if (num == numN + 1) return;
+        Console.Write($"{num} ");
+        PrintNumbersBetweenMN(num + 1, numN);
+    }
+    if (numM > numN)
+    {
+        int num = numM;
+        if (num == numN - 1) return;
+        Console.Write($"{num} ");
+        PrintNumbersBetweenMN(num - 1, numN);
+    }
+}
+
+Console.Write("Введите натуральное число M: ");
+int numberM = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите натуральное число N: ");
+int numberN = Convert.ToInt32(Console.ReadLine());
+
+PrintNumbersBetweenMN(numberM, numberN);
+
