862d434dd52b39857098402259724940f06f70b5
Dmitry <rasma1@mail.ru>
2024-03-28 19:56:47 +0300

    Homework_seminar6_Task1

--- Homework_Seminar6/Task1/Program.cs
+++ Homework_Seminar6/Task1/Program.cs
@@ -3,3 +3,15 @@
 // a b c => “abcdef”
 // d e f 
 
+char[,] CharArray = new char[,] { { 'a', 'b', 'c' },{ 'd', 'e', 'f' } };
+string str = "";
+
+for (int i = 0; i < CharArray.GetLength(0); i++)
+{
+    for (int j = 0; j < CharArray.GetLength(1); j++)
+    {
+        str = str + CharArray[i, j];
+    }
+}
+
+Console.WriteLine(str);
